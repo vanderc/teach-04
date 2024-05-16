5a76f27637cc8464531269d887ff368e813d9a93
vanderc <167822234+vanderc@users.noreply.github.com>
2024-05-16 07:45:38 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using teach_04;
+
+Console.WriteLine("\n======================\nSimple Queue\n======================");
+SimpleQueue.Run();
+
+Console.WriteLine("\n======================\nCustomer Service\n======================");
+CustomerService.Run();
+
+// Uncomment and run as you get to the solution part
+// Console.WriteLine("\n======================\nSimple Queue\n======================");
+// SimpleQueueSolution.Run();
+//
+// Console.WriteLine("\n======================\nCustomer Service\n======================");
+// CustomerServiceSolution.Run();
